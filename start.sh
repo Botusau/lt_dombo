3af2ead0f4d8c73650669a64051a426b550c5864
Botusau <Botusau@gmail.com>
2025-03-16 16:52:45 +0300

    Поытка 3

--- start.sh
+++ start.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+if [[ -d ./lt_dombo ]]; then
+    echo "Removing Lock"
+    rm -rf ./lt_dombo
+fi
+
 git clone https://github.com/Botusau/lt_dombo.git
+
+cd ./lt_dombo/
+
 pip install --upgrade pip
 pip install torch --index-url https://download.pytorch.org/whl/cpu
-pip install -r requirements.txt
+pip install -r ./lt_dombo/requirements.txt
+
 uvicorn main:app --host 0.0.0.0 --port 8000
