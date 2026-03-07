5775c17db23f3bffa8e9e02b2a94efb553bff8f5
Botusau <Botusau@gmail.com>
2026-03-07 19:27:40 +0300

    chore(config): remove linear_l2 from default algorithms
    Updated DEFAULT_ALGORITHMS to only include neural network ("nn") as the default model for AutoML tasks.

--- app/config.py
+++ app/config.py
@@ -56,7 +56,7 @@ BERT_LANGUAGE: Final[str] = "ru"
 # Алгоритмы AutoML
 #["linear_l2", "lgb", "cb", "nn", "lgb_tuned", "cb_tuned"]
 DEFAULT_ALGORITHMS: Final[list[list[str]]] = [
-    ["linear_l2", "nn"]
+    ["nn"]
 ]
 
 # CV настройки
