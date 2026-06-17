6e86417a69ce6b37a6542be399772b62413f8e5b
Botusau <Botusau@gmail.com>
2026-06-18 02:45:38 +0300

    1

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 pip install --upgrade pip
 pip install torch --index-url https://download.pytorch.org/whl/cpu
+pip install numpy scipy pandas patsy
+pip install statsmodels
 pip install --no-cache-dir -U -r requirements.txt
 
 uvicorn main:app --host 0.0.0.0 --port 8000
