a5bb6fa48f1c5ab73f270bb804b524b4e85c2629
Botusau <Botusau@gmail.com>
2026-06-20 13:37:41 +0300

    1

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pip install --upgrade pip
-pip install torch --index-url https://download.pytorch.org/whl/cpu
+pip install torch==2.6.0 --index-url https://download.pytorch.org/whl/cpu
 pip install -U -r requirements.txt
 
 uvicorn main:app --host 0.0.0.0 --port 8000
